065c9485a97f7ddaaad89034a277164f5940225a
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2026-03-02 11:15:22 +0700

    feat: implementasi fungsi CRUD pada UserServiceImpl
    - Membuat class UserServiceImpl untuk mengeksekusi logika bisnis dari UserService
    - Menambahkan validasi request pada fungsi AddUser dan UpdateUser
    - Menerapkan auto-generate UUID untuk pembuatan User baru
    - Menerapkan konversi otomatis Entity ke DTO menggunakan UserMapper
    - Menambahkan Exception handling jika ID user tidak ditemukan pada database

--- src/main/java/com/example/demo/model/dto/UserAddRequest.java
+++ src/main/java/com/example/demo/model/dto/UserAddRequest.java
@@ -6,4 +6,5 @@ import lombok.Data;
 public class UserAddRequest {
     private String name;
     private int age;
+
 }
--- src/main/java/com/example/demo/model/entity/User.java
+++ src/main/java/com/example/demo/model/entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jdk.jshell.Snippet;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,4 +26,5 @@ public class User {
 
     @Column(name = "age", nullable = false)
     private int age;
+
 }
